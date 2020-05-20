4dfae1aa4762f3ab01b5da6ec6686aa80dae7240
Anil Saraswati <swamianilsaraswati@gmail.com>
2020-05-21 01:16:32 +0530

    Initial Commit

--- app/src/main/java/com/example/singletonclassexample/MainActivity.java
+++ app/src/main/java/com/example/singletonclassexample/MainActivity.java
@@ -18,36 +18,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //edit text to input name
         editText = findViewById(R.id.editText);
+        //save data button
         saveData = findViewById(R.id.saveData);
+        //get data button
         getData = findViewById(R.id.getData);
+        //clear data button
         clearData = findViewById(R.id.clearData);
 
+        //on save data button click
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //get the name and converts to string
                 String name = editText.getText().toString();
+                //calls setter to set in shared preference
                 SharedPreference.getInstance(getApplicationContext()).setName(name);
                 Toast.makeText(getApplicationContext(),"Data Saved",Toast.LENGTH_LONG).show();
+                //reset the edit text to empty
                 editText.setText("");
             }
         });
 
+        //on get data button click
         getData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //retrieves the name from shared preference using getter
                 String name = SharedPreference.getInstance(getApplicationContext()).getName();
+                //setting to edit text to display to user
                 editText.setText(name);
                 Toast.makeText(getApplicationContext(),"Data Retrieved",Toast.LENGTH_LONG).show();
             }
         });
 
+        //on clear data button click
         clearData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //call clearData()
                 SharedPreference.getInstance(getApplicationContext()).clearData();
                 Toast.makeText(getApplicationContext(),"Data Cleared",Toast.LENGTH_LONG).show();
+                //getting name after clearing the data which will return empty string
                 String name = SharedPreference.getInstance(getApplicationContext()).getName();
+                //setting empty string to edit text
                 editText.setText(name);
             }
         });
--- app/src/main/java/com/example/singletonclassexample/SharedPreference.java
+++ app/src/main/java/com/example/singletonclassexample/SharedPreference.java
@@ -3,16 +3,23 @@ package com.example.singletonclassexample;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+/* shared preference class to be used by the application
+ * which implements singleton class design pattern
+ * */
 public class SharedPreference {
+    //preference name
     private static final String PREFS_NAME = "SingletonClassExample";
     static SharedPreferences sharedPreferences;
     static SharedPreferences.Editor prefEditor = null;
     private static Context context;
     public static SharedPreference instance = null;
 
+    //to create only one instance
     public static SharedPreference getInstance(Context context1){
         context = context1;
+        //checks if first time creating the object or not
         if(instance == null){
+            //creates first time instance
             instance = new SharedPreference();
         }
         sharedPreferences = context1.getSharedPreferences(PREFS_NAME,Context.MODE_PRIVATE);
@@ -20,15 +27,18 @@ public class SharedPreference {
         return instance;
     }
 
+    //setter function for name
     public void setName(String name){
         prefEditor.putString("name",name);
         prefEditor.commit();
     }
 
+    //getter function for name
     public String getName(){
         return sharedPreferences.getString("name","");
     }
 
+    //clears shared preferences
     public void clearData(){
         prefEditor.clear();
         prefEditor.commit();
